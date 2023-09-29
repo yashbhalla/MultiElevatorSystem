702782f308b02ad82e2faf1248dd9dc0dd0615a2
Yash Bhalla <yashbhalla2015@gmail.com>
2023-09-28 22:51:21 -0400

    Code

---
+++ Elevator/Elevator/Elevator.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+public class Elevator
+{
+    public int numberOfElevators = 1;
+    public int[] elevatorFloor;
+    private bool[] floorReady;
+    public int timeTaken = 10;
+    public int myFloor = 0;
+    public string currentFloor;
+    private int topfloor;
+    public ElevatorStatus Status = ElevatorStatus.STOPPED;
+
+    public Elevator(int NumberOfFloors)
+    {
+        floorReady = new bool[NumberOfFloors + 1];
+        topfloor = NumberOfFloors;
+    }
+
+    private void Stop(int floor)
+    {
+        Status = ElevatorStatus.STOPPED;
+        myFloor = floor;
+        floorReady[floor] = false;
+    }
+
+    private void Descend(int floor)
+    {
+        for (int i = myFloor; i >= 1; i--)
+        {
+            if (floorReady[i])
+                Stop(floor);
+            else
+                continue;
+        }
+        Status = ElevatorStatus.STOPPED;
+    }
+
+    private void Ascend(int floor)
+    {
+        for (int i = myFloor; i <= topfloor; i++)
+        {
+            if (floorReady[i])
+                Stop(floor);
+            else
+                continue;
+        }
+        Status = ElevatorStatus.STOPPED;
+    }
+
+    public void StayPut()
+    {
+        Console.WriteLine("That's our current floor");
+    }
+
+    public int NearestElevator(int myFloor)
+    {
+        int i = 0;
+
+        if (numberOfElevators == 1)
+        {
+            i = elevatorFloor[0];
+        }
+        else
+        {
+            int[] findLift = new int[numberOfElevators];
+
+            foreach (int lift in elevatorFloor)
+            {
+                findLift[i] = Math.Abs(lift - myFloor);
+                i++;
+            }
+
+            for (int j = 0; j < findLift.Length - 1; j++)
+            {
+                if (findLift[j] < findLift[j + 1])
+                {
+                    i = j;
+                }
+                else
+                {
+                    i = j + 1;
+                }
+            }
+            i = elevatorFloor[i];
+        }
+
+        Console.WriteLine("\nThe nearest lift to you is at floor {0}", i);
+
+        return i;
+    }
+
+    public void FloorPress(int destFloor, int myFloor, int numberOfElevators)
+    {
+        this.numberOfElevators = numberOfElevators;
+        elevatorFloor = new int[numberOfElevators];
+        int nearestElevator = 0;
+
+        if (destFloor > topfloor)
+        {
+            Console.WriteLine("We only have {0} floors", topfloor);
+            return;
+        }
+
+        floorReady[destFloor] = true;
+
+        for (int i = 0; i < numberOfElevators; i++)
+        {
+            Random random = new Random();
+            elevatorFloor[i] = random.Next(1, topfloor);
+            floorReady[elevatorFloor[i]] = true;
+        }
+
+        for(int i = 0; i < numberOfElevators; i++)
+            Console.WriteLine("\nElevator {0} is at floor {1}", i, elevatorFloor[i]);
+
+        nearestElevator = NearestElevator(myFloor);
+
+        timeTaken = 10;
+
+        if (nearestElevator > myFloor)
+            timeTaken += Math.Abs(nearestElevator - myFloor) * 2;
+        else if (nearestElevator < myFloor)
+            timeTaken += Math.Abs(myFloor - nearestElevator) * 2;
+
+        switch (Status)
+        {
+            case ElevatorStatus.DOWN:
+                timeTaken += Math.Abs(myFloor - destFloor) * 2;
+                Descend(destFloor);
+                Console.WriteLine("\nDestination Reached! Total time taken to reach your destination: {0} \n\n", timeTaken);
+                break;
+
+            case ElevatorStatus.STOPPED:
+                if (myFloor < destFloor)
+                {
+                    timeTaken += Math.Abs(destFloor - myFloor) * 2;
+                    Ascend(destFloor);
+                    Console.WriteLine("\nDestination Reached! Total time taken to reach your destination: {0} \n\n", timeTaken);
+                }
+                else if (myFloor == destFloor)
+                {
+                    StayPut();
+                    Console.WriteLine("\nDestination Reached! Total time taken to reach your destination: {0} \n\n", timeTaken);
+                }
+                else
+                {
+                    timeTaken += Math.Abs(myFloor - destFloor) * 2;
+                    Descend(destFloor);
+                    Console.WriteLine("\nDestination Reached! Total time taken to reach your destination: {0} \n\n", timeTaken);
+                }
+                break;
+
+            case ElevatorStatus.UP:
+                timeTaken += Math.Abs(destFloor - myFloor) * 2;
+                Ascend(destFloor);
+                Console.WriteLine("\nDestination Reached! Total time taken to reach your destination: {0} \n\n", timeTaken);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public enum ElevatorStatus
+    {
+        UP,
+        STOPPED,
+        DOWN
+    }
+}
---
+++ Elevator/Elevator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+
+public class Program
+{
+    private const string QUIT = "N";
+    private const string NEWBUILDING = "Y";
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("=== WORKING OF A SINGLE ELEVATOR ===");
+        Console.WriteLine("A single lift in the building which would take you to your desired floor. " +
+            "It will calculate the time taken for the lift to arrive, open-close, and reach your destination. " +
+            "\nEnjoy!");
+        Console.WriteLine("====================================");
+        Console.WriteLine("");
+
+    Start:
+        int floor = 0;
+        string floorInput = "";
+        Elevator elevator;
+
+        Console.WriteLine("Enter the number of floors in the building: ");
+        floorInput = Console.ReadLine();
+
+        Console.WriteLine("Enter number of elevators in the building: ");
+        string numberOfElevators = Console.ReadLine();
+
+        if (Int32.TryParse(floorInput, out floor))
+            elevator = new Elevator(floor);
+
+        else
+        {
+            Console.WriteLine("Wrong Input! Kindly enter a number!");
+            Console.Beep();
+            Thread.Sleep(2000);
+            Console.Clear();
+            goto Start;
+        }
+
+        string input = string.Empty;
+
+        while (input != QUIT)
+        {
+            Console.WriteLine("Enter the floor you are at: ");
+            elevator.currentFloor = Console.ReadLine();
+            Console.WriteLine("Enter the floor you wish to go to: ");
+            input = Console.ReadLine();
+
+            if (Int32.TryParse(input, out floor) && Int32.TryParse(elevator.currentFloor, out elevator.myFloor) && Int32.TryParse(numberOfElevators, out elevator.numberOfElevators))
+                elevator.FloorPress(floor, elevator.myFloor, elevator.numberOfElevators);
+
+            else if (input == QUIT)
+                Console.WriteLine("GoodBye!");
+
+            else if (input == NEWBUILDING)
+            {
+                Console.WriteLine("\nSTARTING AGAIN...\n");
+                goto Start;
+            }
+
+            else
+                Console.WriteLine("You have pressed an incorrect floor. Please try again!");
+
+            Console.WriteLine("Do you want to continue? (Y/N): ");
+            input = Console.ReadLine();
+        }
+    }
+}
